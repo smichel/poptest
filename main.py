36a7d4a36242c8015485986d61d1b927ad806d81
Michel Simon <michel.simon@ad.cen.uni-hamburg.de>
2019-10-11 10:30:49 +0200

    first test

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+plt.rcParams['animation.ffmpeg_path'] = 'C:/Users/u300675/Downloads/ffmpeg-4.2.1.tar'
+
 from scipy import ndimage
 
 def coriolis(lat,vel_ang=2*np.pi/86400):
@@ -15,6 +19,13 @@ def migration(i,j,pop,biom,landmask):
         biom_s = biom[i, j + 1]
         biom_w = biom[i - 1, j]
         biom_surround= biom[i, j - 1] + biom[i + 1, j] + biom[i, j + 1] + biom[i - 1, j]
+        pop_delta = np.zeros(pop.shape)
+        biom_delta = np.zeros(pop.shape)
+
+        if biom_surround <= 0:
+            pop_delta[i, j] = -pop[i, j]
+            biom_delta[i,j] = biom_res
+            return pop_delta,biom_delta
         biom_weight = np.array([biom[i, j - 1], biom[i + 1, j],  biom[i, j + 1], biom[i - 1, j]]) / biom_surround
 
         pop_delta = np.zeros(pop.shape)
@@ -24,18 +35,19 @@ def migration(i,j,pop,biom,landmask):
         pop_delta[i, j + 1] = pop_res * biom_weight[2]
         pop_delta[i - 1, j] = pop_res * biom_weight[3]
 
-        biom_delta = np.zeros(pop.shape)
         biom_delta[i,j] = biom_res
 
         return pop_delta, biom_delta
     elif pop[i,j]<=biom[i,j]:
+        pop_res = pop[i, j] * .5
 
         biom_res = -pop[i,j]
-
+        if pop[i, j] + pop_res > biom[i,j] + biom_res:
+            pop_res = biom[i,j] + biom_res
         biom_delta = np.zeros(pop.shape)
         biom_delta[i,j] = biom_res
         pop_delta = np.zeros(pop.shape)
-
+        pop_delta[i,j] = pop_res
         return pop_delta,biom_delta
 
 
@@ -58,7 +70,7 @@ height = ndimage.zoom(height,(lats.shape[0]/height.shape[0],lats.shape[1]/height
 dummy = np.random.random((2,4))
 height +=ndimage.zoom(dummy,(lats.shape[0]/dummy.shape[0],lats.shape[1]/dummy.shape[1]))
 height += np.random.random((height.shape))*.1
-#height[height<1.1] = 0
+height[height<0.7] = 0
 height = np.sqrt(height)
 height_nested = np.zeros((lats.shape[0]+2,lats.shape[1]+2))
 height_nested[1:-1,1:-1]=height
@@ -66,8 +78,9 @@ land_mask = height_nested>0
 T_nested = np.zeros((lats.shape[0]+2,lats.shape[1]+2))
 T_nested[1:-1,1:-1] = T
 
-biomass = (T_nested-250)*60 - height_nested * 2000
+biomass = (T_nested-250)*100 - height_nested * 2000
 biomass[biomass<0] = 0
+biomass[height_nested==0] = 0
 pop = np.zeros(biomass.shape)
 pop[np.random.randint(1,pop.shape[0]-1),np.random.randint(1,pop.shape[1]-1)] = 1000
 timesteps = 1000
@@ -80,10 +93,59 @@ for t in range(timesteps):
         pop +=pop_delta
         biomass += biomass_delta
 
-
+    pop[pop<0] = 0
     biomass[biomass<0] = 0
     pop_time[:,:,t] = pop
     biomass_time[:, :, t] = biomass
+
+timesteps =np.where(np.sum(np.sum(pop_time,axis=1),axis=0)==0)[0][0]
+for t in range(timesteps):
+    if t == 0:
+        fig, axes = plt.subplots(2,1)
+        img_biom = axes[0].imshow(biomass_time[:,:,t],cmap=plt.get_cmap('YlGn'))
+        img_pop = axes[1].imshow(pop_time[:, :, t])
+        s_pop = fig.colorbar(img_pop,ax=axes[1])
+       #s_pop.set_clim(0, 2000)
+        #s_pop.set_ticks(np.arange(0,2100,200))
+        s_pop.draw_all()
+        s_pop.set_label('Population')
+        s_bio = fig.colorbar(img_biom,ax=axes[0])
+        #s_bio.set_clim(0, 1000)
+        #s_bio.set_ticks(np.arange(0,1100,100))
+        s_bio.draw_all()
+        s_bio.set_label('Biomasse')
+    else:
+        img_biom.set_data(biomass_time[:,:,t])
+        img_pop.set_data(pop_time[:, :, t])
+    plt.pause(0.05)
+
+fig, axes = plt.subplots(2, 1)
+img_biom = axes[0].imshow(biomass_time[:, :, t], cmap=plt.get_cmap('YlGn'))
+img_pop = axes[1].imshow(pop_time[:, :, t])
+s_pop = fig.colorbar(img_pop, ax=axes[1])
+# s_pop.set_clim(0, 2000)
+# s_pop.set_ticks(np.arange(0,2100,200))
+s_pop.draw_all()
+s_pop.set_label('Population')
+s_bio = fig.colorbar(img_biom, ax=axes[0])
+# s_bio.set_clim(0, 1000)
+# s_bio.set_ticks(np.arange(0,1100,100))
+s_bio.draw_all()
+s_bio.set_label('Biomasse')
+
+def animate(t):
+    img_biom.set_data(biomass_time[:, :, t])
+    img_pop.set_data(pop_time[:, :, t])
+    return [img_biom,img_pop]
+
+
+anim = animation.FuncAnimation(fig, animate,
+                               frames=timesteps,
+                               interval=200, repeat=1,
+                               blit=True)
+anim.save('C:\Users\u300675\poptest', fps=5,
+          extra_args=['-vcodec', 'h264',
+                      '-pix_fmt', 'yuv420p'])
 
 for t in range(timesteps):
     if t == 0:
@@ -96,6 +158,9 @@ for t in range(timesteps):
     else:
         img.set_data(pop_time[:,:,t])
     plt.pause(0.05)
-plt.imshow(biomass)
+    if t%50==0:
+        if (np.sum(biomass_time[:,:,t])==np.sum(biomass_time[:,:,t-1])):
+            t=timesteps
+#plt.imshow(biomass)
 plt.figure()
 plt.imshow(pop)
